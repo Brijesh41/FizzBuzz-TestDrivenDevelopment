1472d46cd8a2defc5eaf5ddeef6897dc22d7e60c
Brijesh41 <brijesh.shah41@gmail.com>
2020-09-04 16:19:57 +0530

    Added a filter of nonnegative and non-zero numbers

---
+++ Tests/FizzBuzz.java
@@ -0,0 +1,6 @@
+public class FizzBuzz {
+    public String fizzbuzzer(int number){
+        if(number<=0) return "Invalid Input";
+        return null;
+    }
+}
--- Tests/FizzBuzzTest.java
+++ Tests/FizzBuzzTest.java
@@ -2,7 +2,11 @@ import org.junit.Assert;
 import org.junit.Test;
 public class FizzBuzzTest{
     @Test
-    public void test(){
+    public void InputValuelessthan(){
 
+        FizzBuzz fb = new FizzBuzz();
+        int number = 0;
+        String Output = fb.fizzbuzzer(number);
+        Assert.assertEquals("Invalid Input",Output);
     }
 }
