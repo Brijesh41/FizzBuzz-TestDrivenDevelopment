018a888b566411dbfea1706ec8fa7ab91224fab4
Brijesh41 <brijesh.shah41@gmail.com>
2020-09-04 16:29:13 +0530

    Added a Test for 1 and 2

--- Tests/FizzBuzz.java
+++ Tests/FizzBuzz.java
@@ -1,6 +1,10 @@
 public class FizzBuzz {
     public String fizzbuzzer(int number){
         if(number<=0) return "Invalid Input";
+
+        if(number==1) return Integer.toString(1);
+
+        if(number==2) return Integer.toString(2);
         return null;
     }
 }
--- Tests/FizzBuzzTest.java
+++ Tests/FizzBuzzTest.java
@@ -2,11 +2,26 @@ import org.junit.Assert;
 import org.junit.Test;
 public class FizzBuzzTest{
     @Test
-    public void InputValuelessthan(){
+    public void InputValuelessthanOne(){
 
         FizzBuzz fb = new FizzBuzz();
         int number = 0;
         String Output = fb.fizzbuzzer(number);
         Assert.assertEquals("Invalid Input",Output);
+    }
+
+    @Test
+    public void InputValueisOne(){
+        FizzBuzz fb = new FizzBuzz();
+        int number = 1;
+        String Output = fb.fizzbuzzer(number);
+        Assert.assertEquals("1",Output);
+    }
+    @Test
+    public void InputValueisTwo(){
+        FizzBuzz fb = new FizzBuzz();
+        int number = 2;
+        String Output = fb.fizzbuzzer(number);
+        Assert.assertEquals("2",Output);
     }
 }
